80c2953b74883cedfbbf8190bc0a037f76fd1988
yhao3 <kyrieeeee2@gmail.com>
2022-09-05 23:58:21 +0800

    feat: add AList

---
+++ src/lists/alist/AList.java
@@ -0,0 +1,50 @@
+package lists.alist;
+
+/** 
+ * Array based list.
+ * <pre>
+ * Invariants (things that must be true): 
+ * 1. The position of the next item to be inserted (using addLast) is always size.
+ * 2. The number of items in the AList is always size.
+ * 3. The position of the last item in the list is always (size - 1).
+ * </pre>
+ */
+public class AList {
+    private int[] items;
+    private int size;
+    
+    /** Creates an empty list. */
+    public AList() {
+        items = new int[100];
+        size = 0;
+    }
+
+    /** Inserts X into the back of the list. */
+    public void addLast(int x) {
+        items[size] = x;
+        size++;
+    }
+
+    /** Returns the item from the back of the list. */
+    public int getLast() {
+        return items[size - 1];
+    }
+    /** Gets the ith item in the list (0 is the front). */
+    public int get(int i) {
+        return items[i];
+    }
+
+    /** Returns the number of items in the list. */
+    public int size() {
+        return size;
+    }
+
+    /** Deletes item from back of the list and
+      * returns deleted item. */
+    public int removeLast() {
+        int temp = items[size - 1];
+        items[size - 1] = 0; // Setting deleted item to zero is not necessary to preserve invariants, and thus not necessary for correctness.
+        size--;
+        return temp;
+    }
+} 
